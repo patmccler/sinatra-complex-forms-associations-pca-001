d2adadbe5fca09d5ff4572a07ace01e413b77991
Pat McClernan <patmccler@gmail.com>
2020-06-09 11:33:43 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,24 +2,46 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
+    owner = Owner.find(params[:pet_owner_id]) if params[:pet_owner_id]
+    pet = Pet.create(name: params[:pet_name])
 
-    redirect to "pets/#{@pet.id}"
+    owner = Owner.create(name: params["owner_name"]) unless params["owner_name"].empty?
+
+    owner.pets << pet if owner
+
+    redirect to "pets/#{pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
+
+  patch '/pets/:id' do
+
+    pet = Pet.find(params[:id])
+    pet.update(name: params[:pet_name])
+
+    owner = Owner.find(params[:pet_owner_id]) if params[:pet_owner_id]
+
+    owner = Owner.create(params[:owner]) if params[:owner] && params[:owner][:name] != ""
+
+    owner.pets << pet if owner
 
     redirect to "pets/#{@pet.id}"
   end
